98c8223f4652a3b5d3f547da75f9447504f9e1b4
carol <caroltatianacely@gmail.com>
2023-02-12 17:00:54 -0500

    Avanzando primera parte

--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -35,7 +35,11 @@ public class Producer extends Thread {
             dataSeed = dataSeed + rand.nextInt(100);
             System.out.println("Producer added " + dataSeed);
             queue.add(dataSeed);
-            
+
+            if(queue.size() > 0){
+                int elem = queue.poll();
+                System.out.println("Consumer consumes" + elem);
+            }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
